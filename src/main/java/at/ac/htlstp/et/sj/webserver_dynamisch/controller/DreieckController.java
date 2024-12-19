0c34ed9930321175b1bb2ad1c8e8d20902ccf188
Leander Klik <160396774+LEGOlas06@users.noreply.github.com>
2024-12-19 11:25:51 +0100

    Funktioniert

--- src/main/java/at/ac/htlstp/et/sj/webserver_dynamisch/controller/DreieckController.java
+++ src/main/java/at/ac/htlstp/et/sj/webserver_dynamisch/controller/DreieckController.java
@@ -9,25 +9,25 @@ import org.springframework.web.bind.annotation.*;
 public class DreieckController {
 
     @GetMapping("/")
-    public String showForm(Model model) {
+    public String FormZeigen(Model model) {
         model.addAttribute("dreieckForm", new DreieckFormDto());
         return "index";
     }
 
     @PostMapping("/analyze")
-    public String analyzeTriangle(@ModelAttribute DreieckFormDto dreieckForm, Model model) {
+    public String dreieckAnalysieren(@ModelAttribute DreieckFormDto dreieckForm, Model model) {
         double seiteA = dreieckForm.getSeiteA();
         double seiteB = dreieckForm.getSeiteB();
         double seiteC = dreieckForm.getSeiteC();
 
         if (seiteA <= 0 || seiteB <= 0 || seiteC <= 0) {
-            model.addAttribute("error", "The given sides do not form a valid triangle.");
+            model.addAttribute("error", "Kein Dreieck");
             return "index";
         }
 
         double s = (seiteA + seiteB + seiteC) / 2;
-        double area = Math.sqrt(s * (s - seiteA) * (s - seiteB) * (s - seiteC));
-        double perimeter = seiteA + seiteB + seiteC;
+        double flaeche = Math.sqrt(s * (s - seiteA) * (s - seiteB) * (s - seiteC));
+        double umfang = seiteA + seiteB + seiteC;
 
         double winkelA = Math.toDegrees(Math.acos((seiteB * seiteB + seiteC * seiteC - seiteA * seiteA) / (2 * seiteB * seiteC)));
         double winkelB = Math.toDegrees(Math.acos((seiteA * seiteA + seiteC * seiteC - seiteB * seiteB) / (2 * seiteA * seiteC)));
@@ -36,18 +36,18 @@ public class DreieckController {
         model.addAttribute("seiteA", seiteA);
         model.addAttribute("seiteB", seiteB);
         model.addAttribute("seiteC", seiteC);
-        model.addAttribute("area", area);
-        model.addAttribute("perimeter", perimeter);
+        model.addAttribute("flaeche", flaeche);
+        model.addAttribute("umfang", umfang);
         model.addAttribute("winkelA", winkelA);
         model.addAttribute("winkelB", winkelB);
         model.addAttribute("winkelC", winkelC);
 
-        return "result";
+        return "ergebnisse";
     }
 
     @PostMapping("/determine")
     @ResponseBody
-    public String determineTriangle(@RequestBody DreieckFormDto dreieckForm) {
+    public String DreiecksErkennung(@RequestBody DreieckFormDto dreieckForm) {
         double seiteA = dreieckForm.getSeiteA();
         double seiteB = dreieckForm.getSeiteB();
         double seiteC = dreieckForm.getSeiteC();
